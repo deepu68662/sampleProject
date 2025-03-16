492c362a0bd61d7538c3fd38e8ecc6c19c52f8ce
deepthi kompalli <deepu.kompalli@gmail.com>
2025-03-16 23:25:40 +0530

    client-project changes

--- samplePractice/src/app/app.routes.ts
+++ samplePractice/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { EmployeeComponent } from './components/employee/employee.component';
 import { ClientComponent } from './components/client/client.component';
+import { ClientProjectComponent } from './components/client-project/client-project.component';
 
 export const routes: Routes = [
     {path:"",redirectTo:"HomeComponent",pathMatch:"full"},
     {path:"employee",component:EmployeeComponent},
-    {path:"client",component:ClientComponent}
+    {path:"client",component:ClientComponent},
+    {path:"clientproject",component:ClientProjectComponent}
 ];
--- samplePractice/src/app/components/client-project/client-project.component.ts
+++ samplePractice/src/app/components/client-project/client-project.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-client-project',
-  imports: [],
+  imports: [ReactiveFormsModule,FormsModule],
   templateUrl: './client-project.component.html',
   styleUrl: './client-project.component.css'
 })
 export class ClientProjectComponent {
 
+  projectForm:FormGroup = new FormGroup({
+    clientProjectId : new FormControl(""),
+    projectName : new FormControl(""),
+    startDate : new FormControl(""),
+    expectedEndDate : new FormControl(""),
+    leadByEmpId : new FormControl(""),
+    completedDate : new FormControl(""),
+    contactPerson : new FormControl(""),
+
+    contactPersonContactNo : new FormControl(""),
+    totalEmpWorking : new FormControl(""),
+    projectCost : new FormControl(""),
+    projectDetails : new FormControl(""),
+
+    contactPersonEmailId : new FormControl(""),
+    clientId : new FormControl(""),
+
+  })
 }
